5e20aba029a62e9f506c6a696482bc323565586d
Iron Brain <wuopcarlos@gmail.com>
2018-03-17 18:38:48 -0400

    add statistical_outlier_filter

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -38,6 +38,7 @@ def make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose)
     yaml_dict["object_name"] = object_name.data
     yaml_dict["pick_pose"] = message_converter.convert_ros_message_to_dictionary(pick_pose)
     yaml_dict["place_pose"] = message_converter.convert_ros_message_to_dictionary(place_pose)
+    #print(yaml_dict)
     return yaml_dict
 
 # Helper function to output to yaml file
@@ -46,105 +47,41 @@ def send_to_yaml(yaml_filename, dict_list):
     with open(yaml_filename, 'w') as outfile:
         yaml.dump(data_dict, outfile, default_flow_style=False)
 
+#def statistical_outlier_filter(cloud_data, mean=50, deviation=1.0):
+#	outlier_filter = cloud_data.make_statistical_outlier_filter()
+#	outlier_filter.set_mean_k(mean)
+#	outlier_filter.set_std_dev_mul_thresh(deviation)
+#	cloud_filtered = outlier_filter.filter()
+#	return cloud_filtered
+
+
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
-
 # Exercise-2 TODOs:
-
-    # TODO: Convert ROS msg to PCL data
     
-    # TODO: Statistical Outlier Filtering
-
-    # TODO: Voxel Grid Downsampling
-
-    # TODO: PassThrough Filter
-
-    # TODO: RANSAC Plane Segmentation
-
-    # TODO: Extract inliers and outliers
-
-    # TODO: Euclidean Clustering
-
-    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
-
-    # TODO: Convert PCL data to ROS messages
-
-    # TODO: Publish ROS messages
 
 # Exercise-3 TODOs:
+    
 
-    # Classify the clusters! (loop through each detected cluster one at a time)
-
-        # Grab the points for the cluster
-
-        # Compute the associated feature vector
-
-        # Make the prediction
-
-        # Publish a label into RViz
-
-        # Add the detected object to the list of detected objects.
-
-    # Publish the list of detected objects
-
-    # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
-    # Could add some logic to determine whether or not your object detections are robust
-    # before calling pr2_mover()
     try:
-        pr2_mover(detected_objects_list)
+        pr2_mover(detected_objects)
     except rospy.ROSInterruptException:
         pass
 
+
+
 # function to load parameters and request PickPlace service
 def pr2_mover(object_list):
-
-    # TODO: Initialize variables
-
-    # TODO: Get/Read parameters
-
-    # TODO: Parse parameters into individual variables
-
-    # TODO: Rotate PR2 in place to capture side tables for the collision map
-
-    # TODO: Loop through the pick list
-
-        # TODO: Get the PointCloud for a given object and obtain it's centroid
-
-        # TODO: Create 'place_pose' for the object
-
-        # TODO: Assign the arm to be used for pick_place
-
-        # TODO: Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
-
-        # Wait for 'pick_place_routine' service to come up
-        rospy.wait_for_service('pick_place_routine')
-
-        try:
-            pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
-
-            # TODO: Insert your message variables to be sent as a service request
-            resp = pick_place_routine(TEST_SCENE_NUM, OBJECT_NAME, WHICH_ARM, PICK_POSE, PLACE_POSE)
-
-            print ("Response: ",resp.success)
-
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
-
-    # TODO: Output your request parameters into output yaml file
-
+  	
 
 
 if __name__ == '__main__':
 
-    # TODO: ROS node initialization
-
-    # TODO: Create Subscribers
-
-    # TODO: Create Publishers
-
-    # TODO: Load Model From disk
+    
 
     # Initialize color_list
     get_color_list.color_list = []
 
     # TODO: Spin while node is not shutdown
+    while not rospy.is_shutdown():
+		rospy.spin()
